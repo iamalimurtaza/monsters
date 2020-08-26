6f934968d00db2cf2fcf64dd5b6164cef243e039
iamalimurtaza <ali@badoatech.com>
2020-08-27 01:58:03 +0500

    Updates

---
+++ precache-manifest.03f224d7d7f3b8644e88b183dac626e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9571a8d876782f059a926d3fc3becc",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "2e17efd797ec2f9846ae",
+    "url": "/monsters/static/css/main.8faa7fad.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e17efd797ec2f9846ae",
+    "url": "/monsters/static/js/main.de6331ed.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
